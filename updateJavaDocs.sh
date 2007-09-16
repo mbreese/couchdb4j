fba21f80b0b3f8c5c88fc621258739f236c8d577
mbreese <mbreese@837bb385-1c3a-0410-a21b-0ff572576420>
2007-09-16 19:31:54 +0000

    Removed old Javadocs.

--- updateJavaDocs.sh
+++ updateJavaDocs.sh
@@ -4,4 +4,8 @@ svn rm javadoc
 svn ci -m "Removed old Javadocs."
 ant javadoc
 svn add javadoc
+
+find javadoc -name *html -exec svn propset svn:mime-type text/html \{\} \;
+find javadoc -name *gif -exec svn propset svn:mime-type image/gif \{\} \;
+
 svn ci -m "Added updated Javadocs."
